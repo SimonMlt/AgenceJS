c7ef40e7d2f43765d9d8cf5376d79a3311ce7b66
Jeevesen Curpen <jorvencurpen92@gmail.com>
2020-09-25 16:31:40 +0200

    homepage responsive pb

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,4 +1,5 @@
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleIndicators" class="carousel slide mt-3 ml-auto mr-auto" data-ride="carousel">
+
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -6,13 +7,13 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="d-block w-100" src="storage/img/carousel/villa.jpg" alt="First slide">
+            <img class="d-block w-100" src="storage/img/villa.jpg" alt="First slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="storage/img/carousel/maison.jpg" alt="Second slide">
+            <img class="d-block w-100" src="storage/img/maison.jpg" alt="Second slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="storage/img/carousel/appartement.jpg" alt="Third slide">
+            <img class="d-block w-100" src="storage/img/appartement.jpg" alt="Third slide">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,13 +3,15 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    {{-- <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>@yield('title', 'Mon blog')</title> --}}
+     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+     <title>@yield('title', 'AgenceJS')</title>
 </head>
 <body>
     @include('components.navigation')
-    <div class="container">
+    <div class="container-fluid">
+
         @yield('content')
+
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
