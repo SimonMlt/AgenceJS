0bf21cef52e1735310b7be10b79aef95bf0a1305
Jeevesen Curpen <jorvencurpen92@gmail.com>
2020-09-25 15:22:20 +0200

    migration et carousel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('adresse');
+            $table->string('ville');
+            $table->string('cp');
+            $table->date('date_naissance');
+            $table->string('telephone');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
