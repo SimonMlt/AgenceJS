1414f288b15e362568922cca1805558220b8253e
Jeevesen Curpen <jorvencurpen92@gmail.com>
2020-09-25 15:25:25 +0200

    migration et carousel

---
+++ database/migrations/2020_09_25_121628_create_categorie_habitation_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategorieHabitationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorie_habitation', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorie_habitation');
+    }
+}
---
+++ database/migrations/2020_09_25_121750_create_habitation_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHabitationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habitation', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->text('description');
+            $table->string('ville');
+            $table->string('cp');
+            $table->boolean('cuisine');
+            $table->boolean('salle_de_bain');
+            $table->boolean('toilettes');
+            $table->integer('nb_pieces');
+            $table->integer('nb_chambres');
+            $table->float('surface');
+            $table->integer('annee');
+            $table->float('prix');
+            $table->string('image1');
+            $table->string('image2')->nullable();
+            $table->string('image3')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('habitation');
+    }
+}
