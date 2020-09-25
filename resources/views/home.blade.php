f7e532409af5e88c2d52b5b22883d0d3e9fa132f
SimonMlt <simon-malet@hotmail.fr>
2020-09-25 13:47:05 +0200

    navbar logo et homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+
+@section('title') Accueil @endsection
+
+@section('content')
+    <h1>Mon super blog</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus beatae distinctio ipsum iste libero, minus provident quibusdam tempora velit! Eaque explicabo id non odio, officia omnis quisquam ratione soluta ut.</p>
+
+@endsection
