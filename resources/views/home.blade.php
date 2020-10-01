78f10830c2b4ba553961b7e7f195e2ea7bf2d1af
SimonMlt <simon-malet@hotmail.fr>
2020-10-01 18:00:00 +0200

    responsive design accueil + footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,31 +4,57 @@
 
 @section('content')
 
-    <h1 class="titre_accueil">Bienvenue sur votre Agence</h1>
+    <h1 class="m-auto text-center">Bienvenue chez l'AgenceJS</h1>
 
     <div class="row">
-        <div class="col">
+        <div class="col-md-12 col-sm-12">
             @include('components.carousel')
         </div>
+    </div>
 
-        <div class="col">
+    <div class="row">
+        <p class="text-justify mt-5 ml-auto mr-auto w-75">
+            Au sein de l'AgenceJS, vous pouvez retrouver tous les logements que nous proposons. Appartement, maison ou bien villa,
+            vous avez l'embaras du choix. N'hésitez pas : rentrez votre code postal, et naviguez sur notre site pour découvrir
+            les logements disponibles dans votre département. N'hésitez pas à nous contacter en cas de problèmes,
+            nous nous tenons à votre entière disposition.
+        </p>
+    </div>
+
+    <div class="row">
+        <h2 class="col-md-12 m-auto text-center">Nos dernières annonces :</h2>
+
+        <div class="mt-5 col-md-4 col-sm-12">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="storage/img/appartement.jpg" alt="Card image cap">
+                <img src="storage/img/appartement.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 class="card-title">Appartement</h5>
+                    <p class="card-text">Exemple d'une annonce d'appartement proposé par l'AgenceJS pour ses clients.</p>
+                    <a href="#" class="btn btn-primary">Voir l'annonce</a>
                 </div>
             </div>
         </div>
 
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="storage/img/appartement.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
+        <div class="mt-5 col-md-4 col-sm-12">
+            <div class="card" style="width: 18rem;">
+                <img src="storage/img/maison.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Maison</h5>
+                    <p class="card-text">Exemple d'une annonce de maison proposé par l'AgenceJS pour ses clients.</p>
+                    <a href="#" class="btn btn-primary">Voir l'annonce</a>
                 </div>
+            </div>
+        </div>
+
+        <div class="mt-5 col-md-4 col-sm-12">
+            <div class="card" style="width: 18rem;">
+                <img src="storage/img/villa.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Villa</h5>
+                    <p class="card-text">Exemple d'une annonce de villa proposé par l'AgenceJS pour ses clients.</p>
+                    <a href="#" class="btn btn-primary">Voir l'annonce</a>
+                </div>
+            </div>
         </div>
     </div>
 
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,14 +8,59 @@
 </head>
 <body>
     @include('components.navigation')
-    <div class="container-fluid">
+    <div class="container">
 
         @yield('content')
 
     </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
 </body>
+
+<footer>
+    <div class="bg-secondary text-white mt-5">
+        <div class="container">
+            <div class="row mt">
+                <div class="col-md-3 mt-3 footer-about wow fadeInUp animated" style="visibility: visible; animation-name: fadeInUp;">
+                    <img class="logo-footer" src="storage/img/logo.png" alt="logo-footer" data-at2x="assets/img/logo.png" height="85">
+                    <p>
+                        We are a young company always looking for new and creative ideas to help you with our products in your everyday work.
+                    </p>
+                    <p><a href="#">Our Team</a></p>
+                </div>
+                <div class="col-md-4 mt-3 offset-md-1 footer-contact wow fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;">
+                    <h3>Contact</h3>
+                    <p><i class="fas fa-map-marker-alt"></i> Via Rossini 10, 10136 Turin Italy</p>
+                    <p><i class="fas fa-phone"></i> Phone: (0039) 333 12 68 347</p>
+                    <p><i class="fas fa-envelope"></i> Email: <a href="mailto:hello@domain.com">hello@domain.com</a></p>
+                    <p><i class="fab fa-skype"></i> Skype: you_online</p>
+                </div>
+                <div class="col-md-4 mt-3 footer-links wow fadeInUp animated" style="visibility: visible; animation-name: fadeInUp;">
+                    <div class="row">
+                        <div class="col">
+                            <h3>Links</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p><a class="scroll-link" href="#top-content">Home</a></p>
+                            <p><a href="#">Features</a></p>
+                            <p><a href="#">How it works</a></p>
+                            <p><a href="#">Our clients</a></p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><a href="#">Plans &amp; pricing</a></p>
+                            <p><a href="#">Affiliates</a></p>
+                            <p><a href="#">Terms</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </html>
